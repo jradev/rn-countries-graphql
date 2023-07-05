f0788376341efa46e1ad425a93f70f4c986c510b
Jomari Iringan <jom.iringan@gmail.com>
2023-07-05 21:55:01 +0800

    chore: package design utility & board setup

---
+++ src/views/search-board/index.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// @ts-nocheck
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
+import { Box, Text } from 'react-native-design-utility';
+
+
+export default function SearchBoard(): JSX.Element {
+
+  return (
+    <React.Fragment>
+      <Box>
+        <Text>This is a text</Text>
+      </Box>
+    </React.Fragment>
+  );
+}
